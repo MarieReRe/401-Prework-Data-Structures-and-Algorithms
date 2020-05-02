3681875ea471ba898782dfbf2cbb991c85e32d02
Marie Antons <chefantons@gmail.com>
2020-05-01 19:32:27 -0500

    set up function to ask user for 5 number inputs between 1-10

--- Pre-work Code Challenges/Program.cs
+++ Pre-work Code Challenges/Program.cs
@@ -6,11 +6,24 @@ namespace Pre_work_Code_Challenges
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Challenge 1: Array Max Result");
             arrayMaxResult();
         }
         //PROBLEM 1: Array Max Result: Not a guessing game after all! 
         private static void arrayMaxResult()
         {
+            // 1st step: Request 5 numbers between 1-10 from User
+            // set array length
+            int arrayLength = 5;
+            int[] userInputArray = new int[arrayLength];
+            //Use for loop to ask user for 5 inputs 
+            for(int i = 0; i <arrayLength; i++)
+            {
+                //ask user for input
+                Console.WriteLine("Please enter a number between 1-10, we will ask you this question 5 times. ");
+                // Next we need to put those numbers into the array- we will need to parse into integer for this
+                userInputArray[i] = int.Parse(Console.ReadLine());
+            }
            
                 
                     
